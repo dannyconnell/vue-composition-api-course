f2d8dfc9b3607ac29211dc70eae24572d7d06498
Danny Connell <sendstufftodanny@gmail.com>
2022-02-08 09:57:28 +0000

    3.1

--- vue-composition-api-basics/src/views/HomeView.vue
+++ vue-composition-api-basics/src/views/HomeView.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    
+    <div>
+      <button class="btn">-</button>
+      <span class="counter">0</span>
+      <button class="btn">+</button>
+    </div>
+
   </div>
 </template>
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+.btn, .counter {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
