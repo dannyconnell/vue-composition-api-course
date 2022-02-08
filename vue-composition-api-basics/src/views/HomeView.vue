0523c2c8238e821f05918327f70cd0ea93b3f6fa
Danny Connell <sendstufftodanny@gmail.com>
2022-02-08 15:03:17 +0000

    6.3

--- vue-composition-api-basics/src/views/HomeView.vue
+++ vue-composition-api-basics/src/views/HomeView.vue
@@ -24,7 +24,7 @@
 </template>
 
 <script setup>
-import { reactive, computed, watch, onBeforeMount, onMounted, onBeforeUnmount, onUnmounted, onActivated, onDeactivated } from 'vue'
+import { reactive, computed, watch, onBeforeUpdate, onUpdated } from 'vue'
 
 const appTitle = 'My Ok Counter App'
 
@@ -52,23 +52,11 @@ const decreaseCounter = amount => {
   counterData.count -= amount
 }
 
-onBeforeMount(() => {
-  console.log('onBeforeMount')
+onBeforeUpdate(() => {
+  console.log('onBeforeUpdate')
 })
-onMounted(() => {
-  console.log('onMounted')
-})
-onBeforeUnmount(() => {
-  console.log('onBeforeUnmount')
-})
-onUnmounted(() => {
-  console.log('onUnmounted')
-})
-onActivated(() => {
-  console.log('onActivated')
-})
-onDeactivated(() => {
-  console.log('onDeactivated')
+onUpdated(() => {
+  console.log('onUpdated')
 })
 
 </script>
