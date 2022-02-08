a32b15a6b0b46c30c346eb002d32dc084aa2d895
Danny Connell <sendstufftodanny@gmail.com>
2022-02-08 10:54:34 +0000

    4.1

--- vue-composition-api-basics/src/views/HomeView.vue
+++ vue-composition-api-basics/src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     
+    <h3>{{ counterTitle }}:</h3>
+
     <div>
       <button @click="decreaseCounter" class="btn">-</button>
       <span class="counter">{{ counter }}</span>
@@ -13,7 +15,8 @@
 <script setup>
 import { ref } from 'vue'
 
-const counter = ref(0)
+const counter = ref(0),
+      counterTitle = ref('My Counter')
 
 const increaseCounter = () => {
   counter.value++
