56a0d8fb50cec1490df750450346d82a28018364
Danny Connell <sendstufftodanny@gmail.com>
2022-02-09 15:22:34 +0000

    10.1

---
+++ vue-composition-api-basics/src/components/Modal.vue
@@ -0,0 +1,24 @@
+<template>
+  <teleport to=".modals-container">
+    <div
+      class="modal"
+    >
+      <h1>This is a modal</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae ipsa laboriosam vero natus ut rerum quaerat, saepe praesentium tempore et hic velit odio nemo minus labore quam ullam quod architecto?</p>
+      <button>Hide modal</button>
+    </div>
+  </teleport>
+</template>
+
+<style>
+.modal {
+  background: beige;
+  padding: 10px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+}
+</style>
--- vue-composition-api-basics/src/views/ModalsView.vue
+++ vue-composition-api-basics/src/views/ModalsView.vue
@@ -2,16 +2,7 @@
   <div class="modals">
     <h1>Modals</h1>
     <button @click="showModal = true">Show modal</button>
-    <teleport to=".modals-container">
-      <div
-        v-if="showModal"
-        class="modal"
-      >
-        <h1>This is a modal</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae ipsa laboriosam vero natus ut rerum quaerat, saepe praesentium tempore et hic velit odio nemo minus labore quam ullam quod architecto?</p>
-        <button @click="showModal = false">Hide modal</button>
-      </div>
-    </teleport>
+    <Modal v-if="showModal"></Modal>
   </div>
 </template>
 
@@ -21,6 +12,7 @@
 */
 
   import { ref } from 'vue'
+  import Modal from '@/components/Modal.vue'
 
 /*
   modals
@@ -30,15 +22,14 @@
 
 </script>
 
-<style>
-.modal {
-  background: beige;
-  padding: 10px;
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 1;
+<!--
+<script>
+import Modal from '@/components/Modal.vue'
+
+export default {
+  components: {
+    Modal
+  }
 }
-</style>
+</script>
+-->
