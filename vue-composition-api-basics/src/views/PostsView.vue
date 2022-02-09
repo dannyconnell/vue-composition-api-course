84c7cc18a80dbd9a8e45f6edc310cfceee7abbda
Danny Connell <sendstufftodanny@gmail.com>
2022-02-09 11:10:10 +0000

    9.1

--- vue-composition-api-basics/src/views/PostsView.vue
+++ vue-composition-api-basics/src/views/PostsView.vue
@@ -3,14 +3,11 @@
     <h1>Posts</h1>
 
     <ul>
-      <li>
-        <RouterLink to="/postDetail/id1">Post 1</RouterLink>
-      </li>
-      <li>
-        <RouterLink to="/postDetail/id2">Post 2</RouterLink>
-      </li>
-      <li>
-        <RouterLink to="/postDetail/id3">Post 3</RouterLink>
+      <li
+        v-for="post in posts"
+        :key="post.id"
+      >
+        <RouterLink :to="`/postDetail/${ post.id }`">{{ post.title }}</RouterLink>
       </li>
     </ul>
 
@@ -23,7 +20,27 @@
   imports
 */
 
+  import { ref } from 'vue'
   import { vAutofocus } from '@/directives/vAutofocus'
+
+/*
+  posts
+*/
+
+  const posts = ref([
+    {
+      id: 'id1',
+      title: 'Post 1'
+    },
+    {
+      id: 'id2',
+      title: 'Post 2'
+    },
+    {
+      id: 'id3',
+      title: 'Post 3'
+    }
+  ])
 
 </script>
 
