d349ab5efe1d8a43db3bedff306506fbfaa3d13d
Danny Connell <sendstufftodanny@gmail.com>
2022-02-09 10:41:09 +0000

    8.4

--- vue-composition-api-basics/src/views/PostDetailView.vue
+++ vue-composition-api-basics/src/views/PostDetailView.vue
@@ -5,16 +5,38 @@
     <div>
       <button @click="showPostId">Show Post ID</button>
     </div>
+    <div>
+      <button @click="goHomeIn3Seconds">Go Home in 3 seconds</button>
+    </div>
+    <div>
+      <button @click="goToFirstPost">Go to first post</button>
+    </div>
     <p><RouterLink to="/posts">&lt; Back</RouterLink></p>
   </div>
 </template>
 
 <script setup>
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 
 const route = useRoute()
+const router = useRouter()
 
 const showPostId = () => {
   alert(`The ID of this post is: ${ route.params.id }`)
+}
+
+const goHomeIn3Seconds = () => {
+  setTimeout(() => {
+    router.push({ name: 'home' })
+  }, 3000)
+}
+
+const goToFirstPost = () => {
+  router.push({
+    name: 'postDetail',
+    params: {
+      id: 'id1'
+    }
+  })
 }
 </script>
