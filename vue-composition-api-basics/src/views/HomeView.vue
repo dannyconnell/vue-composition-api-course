9a22bc598712ec541d8a3af582165875d17898e5
Danny Connell <sendstufftodanny@gmail.com>
2022-02-09 11:22:24 +0000

    9.2

--- vue-composition-api-basics/src/views/HomeView.vue
+++ vue-composition-api-basics/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     
-    <h2>{{ appTitle }}</h2>
+    <h2 ref="appTitleRef">{{ appTitle }}</h2>
 
     <h3>{{ counterData.title }}:</h3>
 
@@ -28,7 +28,7 @@
   imports
 */
 
-  import { reactive, computed, watch, onMounted } from 'vue'
+  import { ref, reactive, computed, watch, onMounted } from 'vue'
   import { vAutofocus } from '@/directives/vAutofocus'
 
 /*
@@ -37,8 +37,10 @@
 
   const appTitle = 'My Ok Counter App'
 
+  const appTitleRef = ref(null)
+
   onMounted(() => {
-    console.log('Do stuff related to App Title')
+    console.log(`The app title is ${ appTitleRef.value.offsetWidth } px wide!`)
   })
 
 /*
