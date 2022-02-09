3877d80aa1e8aca85d1bd957e62cce85e31a4002
Danny Connell <sendstufftodanny@gmail.com>
2022-02-09 10:28:54 +0000

    8.3

--- vue-composition-api-basics/src/views/PostDetailView.vue
+++ vue-composition-api-basics/src/views/PostDetailView.vue
@@ -2,10 +2,19 @@
   <div class="post-detail">
     <h1>This is a post page</h1>
     <p>Display the content of post with ID of {{ $route.params.id }} here!</p>
+    <div>
+      <button @click="showPostId">Show Post ID</button>
+    </div>
     <p><RouterLink to="/posts">&lt; Back</RouterLink></p>
   </div>
 </template>
 
 <script setup>
+import { useRoute } from 'vue-router'
 
+const route = useRoute()
+
+const showPostId = () => {
+  alert(`The ID of this post is: ${ route.params.id }`)
+}
 </script>
