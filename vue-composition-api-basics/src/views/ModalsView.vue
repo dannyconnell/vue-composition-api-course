4d04662b3f17a2bec9b3e6161fe5e7fa113bc8c9
Danny Connell <sendstufftodanny@gmail.com>
2022-02-09 12:00:15 +0000

    9.5

--- vue-composition-api-basics/src/views/ModalsView.vue
+++ vue-composition-api-basics/src/views/ModalsView.vue
@@ -2,14 +2,16 @@
   <div class="modals">
     <h1>Modals</h1>
     <button @click="showModal = true">Show modal</button>
-    <div
-      v-if="showModal"
-      class="modal"
-    >
-      <h1>This is a modal</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae ipsa laboriosam vero natus ut rerum quaerat, saepe praesentium tempore et hic velit odio nemo minus labore quam ullam quod architecto?</p>
-      <button @click="showModal = false">Hide modal</button>
-    </div>
+    <teleport to=".modals-container">
+      <div
+        v-if="showModal"
+        class="modal"
+      >
+        <h1>This is a modal</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae ipsa laboriosam vero natus ut rerum quaerat, saepe praesentium tempore et hic velit odio nemo minus labore quam ullam quod architecto?</p>
+        <button @click="showModal = false">Hide modal</button>
+      </div>
+    </teleport>
   </div>
 </template>
 
@@ -32,5 +34,11 @@
 .modal {
   background: beige;
   padding: 10px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
 }
 </style>
