3b3051522596bce03c3392996d627b188c708275
Danny Connell <sendstufftodanny@gmail.com>
2022-02-10 11:38:16 +0000

    11.2

---
+++ vue-composition-api-basics/src/use/useCounter.js
@@ -0,0 +1,40 @@
+import { reactive, computed, watch, onMounted, nextTick } from 'vue'
+
+export function useCounter() {
+  const counterData = reactive({
+    count: 0,
+    title: 'My Counter'
+  })
+
+  watch(() => counterData.count, (newCount) => {
+    if (newCount === 20) {
+      alert('Way to go! You made it to 20!!')
+    }
+  })
+
+  const oddOrEven = computed(() => {
+    if (counterData.count % 2 === 0) return 'even'
+    return 'odd'
+  })
+
+  const increaseCounter = async (amount, e) => {
+    counterData.count += amount
+    await nextTick()
+    console.log('do something when counter has updated in the dom')
+  }
+
+  const decreaseCounter = amount => {
+    counterData.count -= amount
+  }
+
+  onMounted(() => {
+    console.log('Do stuff related to Counter')
+  })
+
+  return {
+    counterData,
+    oddOrEven,
+    increaseCounter,
+    decreaseCounter
+  }
+}
--- vue-composition-api-basics/src/views/HomeView.vue
+++ vue-composition-api-basics/src/views/HomeView.vue
@@ -47,35 +47,7 @@
   counter
 */
 
-  const counterData = reactive({
-    count: 0,
-    title: 'My Counter'
-  })
 
-  watch(() => counterData.count, (newCount) => {
-    if (newCount === 20) {
-      alert('Way to go! You made it to 20!!')
-    }
-  })
-
-  const oddOrEven = computed(() => {
-    if (counterData.count % 2 === 0) return 'even'
-    return 'odd'
-  })
-
-  const increaseCounter = async (amount, e) => {
-    counterData.count += amount
-    await nextTick()
-    console.log('do something when counter has updated in the dom')
-  }
-
-  const decreaseCounter = amount => {
-    counterData.count -= amount
-  }
-
-  onMounted(() => {
-    console.log('Do stuff related to Counter')
-  })
 
 </script>
 
