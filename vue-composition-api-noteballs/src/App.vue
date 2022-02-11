2c7380bc06766ec2a43564290474ac7ad808203a
Danny Connell <sendstufftodanny@gmail.com>
2022-02-11 13:24:03 +0000

    14.2

--- vue-composition-api-noteballs/src/App.vue
+++ vue-composition-api-noteballs/src/App.vue
@@ -1,9 +1,16 @@
 <template>
-  <RouterLink to="/" class="button">Notes</RouterLink> | 
-  <RouterLink to="/stats" class="button">Stats</RouterLink>
+  <NavBar />
 
   <RouterView />
 </template>
+
+<script setup>
+/*
+  imports
+*/
+
+  import NavBar from '@/components/Layout/NavBar.vue'
+</script>
 
 <style>
 @import 'bulma/css/bulma.min.css';
