b2fc120ce18b64446c4fcff15decef065fa4e2f2
Danny Connell <sendstufftodanny@gmail.com>
2022-02-11 11:59:10 +0000

    13.3

--- vue-composition-api-noteballs/src/main.js
+++ vue-composition-api-noteballs/src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHashHistory,
+  routes: []
+})
+
+createApp(App)
+  .use(router)
+  .mount('#app')
