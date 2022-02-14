efdaf32bc701c25a213681e851fe1a29c2d74b59
Danny Connell <sendstufftodanny@gmail.com>
2022-02-14 10:37:04 +0000

    15.3

---
+++ vue-composition-api-noteballs/src/components/Notes/Note.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="card mb-4"
+  >
+    <div class="card-content">
+      <div class="content">
+        {{ note.content }}
+      </div>
+    </div>
+    <footer class="card-footer">
+      <a href="#" class="card-footer-item">Edit</a>
+      <a href="#" class="card-footer-item">Delete</a>
+    </footer>
+  </div>
+</template>
--- vue-composition-api-noteballs/src/views/ViewNotes.vue
+++ vue-composition-api-noteballs/src/views/ViewNotes.vue
@@ -26,21 +26,11 @@
       </div>
     </div>
 
-    <div
+    <Note
       v-for="note in notes"
       :key="note.id"
-      class="card mb-4"
-    >
-      <div class="card-content">
-        <div class="content">
-          {{ note.content }}
-        </div>
-      </div>
-      <footer class="card-footer">
-        <a href="#" class="card-footer-item">Edit</a>
-        <a href="#" class="card-footer-item">Delete</a>
-      </footer>
-    </div>
+    />
+
   </div>
 </template>
 
@@ -51,6 +41,7 @@
 */
 
   import { ref } from 'vue'
+  import Note from '@/components/Notes/Note.vue'
 
 /*
   notes
