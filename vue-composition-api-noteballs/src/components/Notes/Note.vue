49b2d3924e7eb066492a59be1f8f972a69c83484
Danny Connell <sendstufftodanny@gmail.com>
2022-02-14 11:15:15 +0000

    15.5

--- vue-composition-api-noteballs/src/components/Notes/Note.vue
+++ vue-composition-api-noteballs/src/components/Notes/Note.vue
@@ -5,6 +5,9 @@
     <div class="card-content">
       <div class="content">
         {{ note.content }}
+        <div class="has-text-right has-text-grey-light mt-2">
+          <small>{{ characterLength }}</small>
+        </div>
       </div>
     </div>
     <footer class="card-footer">
@@ -15,6 +18,11 @@
 </template>
 
 <script setup>
+/*
+  imports
+*/
+
+  import { computed } from 'vue'
 
 /*
   props
@@ -26,5 +34,16 @@
       required: true
     }
   })
+
+/*
+  character length
+*/
+
+  const characterLength = computed(() => {
+    let length = props.note.content.length
+    let description = length > 1 ? 'characters' : 'character'
+    return `${ length } ${ description }`
+  })
+
 
 </script>
