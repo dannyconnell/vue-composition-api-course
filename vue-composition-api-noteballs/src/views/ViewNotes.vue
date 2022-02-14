000d2b545c5604f1efde4dc6189085dc77d974e9
Danny Connell <sendstufftodanny@gmail.com>
2022-02-14 09:46:40 +0000

    14.7

--- vue-composition-api-noteballs/src/views/ViewNotes.vue
+++ vue-composition-api-noteballs/src/views/ViewNotes.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="notes">
+
+    <div class="card has-background-success-dark p-4 mb-5">
+      <div class="field">
+        <div class="control">
+          <textarea
+            class="textarea"
+            placeholder="Add a new note"
+          />
+        </div>
+      </div>
+
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button class="button is-link has-background-success">
+            Add New Note
+          </button>
+        </div>
+      </div>
+    </div>
+
     <div
       v-for="i in 3"
       class="card mb-4"
