221d0484620c541a246db20678248733b65da965
Danny Connell <sendstufftodanny@gmail.com>
2022-02-14 11:56:52 +0000

    16.1

--- vue-composition-api-noteballs/src/main.js
+++ vue-composition-api-noteballs/src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from '@/router'
 
 createApp(App)
+  .use(createPinia())
   .use(router)
   .mount('#app')
---
+++ vue-composition-api-noteballs/src/stores/storeNotes.js
@@ -0,0 +1,18 @@
+import { defineStore } from 'pinia'
+
+export const useStoreNotes = defineStore('storeNotes', {
+  state: () => {
+    return { 
+      notes: [
+        {
+          id: 'id1',
+          content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem ipsa commodi sint ut ullam culpa nulla molestiae sunt quia qui maxime, enim quasi officiis aperiam fugit, corrupti omnis, eaque animi.'
+        },
+        {
+          id: 'id2',
+          content: 'This is a shorter note! Woo!'
+        }
+      ]
+    }
+  }
+})
