7e85ff98df8069306f9542f586012eb4f40276ec
Danny Connell <sendstufftodanny@gmail.com>
2022-02-14 09:37:14 +0000

    14.6

--- vue-composition-api-noteballs/src/views/ViewNotes.vue
+++ vue-composition-api-noteballs/src/views/ViewNotes.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="notes">
-    <h1>Notes</h1>
+    <div
+      v-for="i in 3"
+      class="card mb-4"
+    >
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem ipsa commodi sint ut ullam culpa nulla molestiae sunt quia qui maxime, enim quasi officiis aperiam fugit, corrupti omnis, eaque animi.
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
   </div>
 </template>
