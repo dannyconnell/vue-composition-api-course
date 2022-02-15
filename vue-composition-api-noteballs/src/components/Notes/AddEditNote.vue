f3884b71742c82d04eb47a09b7c69143ab4bee1b
Danny Connell <sendstufftodanny@gmail.com>
2022-02-15 10:22:17 +0000

    16.7

---
+++ vue-composition-api-noteballs/src/components/Notes/AddEditNote.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card has-background-success-dark p-4 mb-5">
+    <div class="field">
+      <div class="control">
+        <textarea
+          class="textarea"
+          placeholder="Add a new note"
+          ref="newNoteRef"
+        />
+      </div>
+    </div>
+
+    <div class="field is-grouped is-grouped-right">
+      <div class="control">
+        <slot name="buttons" />
+      </div>
+    </div>
+  </div>
+</template>
