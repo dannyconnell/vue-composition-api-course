265f02e0861f67c616f25b6038dc57dc09f77588
Danny Connell <sendstufftodanny@gmail.com>
2022-02-15 10:22:10 +0000

    16.7

--- vue-composition-api-noteballs/src/views/ViewNotes.vue
+++ vue-composition-api-noteballs/src/views/ViewNotes.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="notes">
 
-    <div class="card has-background-success-dark p-4 mb-5">
+    <AddEditNote>
+      <template #buttons>
+        <button
+          @click="addNote"
+          :disabled="!newNote"
+          class="button is-link has-background-success"
+        >
+          Add New Note
+        </button>
+      </template>
+    </AddEditNote>
+
+    <!-- <div class="card has-background-success-dark p-4 mb-5">
       <div class="field">
         <div class="control">
           <textarea
@@ -24,7 +36,7 @@
           </button>
         </div>
       </div>
-    </div>
+    </div> -->
 
     <Note
       v-for="note in storeNotes.notes"
@@ -43,6 +55,7 @@
 
   import { ref } from 'vue'
   import Note from '@/components/Notes/Note.vue'
+  import AddEditNote from '@/components/Notes/AddEditNote.vue'
   import { useStoreNotes } from '@/stores/storeNotes'
 
 /*
