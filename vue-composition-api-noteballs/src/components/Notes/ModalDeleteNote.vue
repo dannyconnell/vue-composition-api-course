975a3c2f5852ed8eb281ca9084b2fb339b7f3435
Danny Connell <sendstufftodanny@gmail.com>
2022-02-17 08:48:26 +0000

    18.2

--- vue-composition-api-noteballs/src/components/Notes/ModalDeleteNote.vue
+++ vue-composition-api-noteballs/src/components/Notes/ModalDeleteNote.vue
@@ -4,15 +4,53 @@
     <div class="modal-card">
       <header class="modal-card-head">
         <p class="modal-card-title">Delete Note?</p>
-        <button class="delete" aria-label="close"></button>
+        <button
+          @click="closeModal"
+          class="delete"
+          aria-label="close"
+        >
+        </button>
       </header>
       <section class="modal-card-body">
         Are you sure you want to delete this note?
       </section>
       <footer class="modal-card-foot is-justify-content-flex-end">
-        <button class="button">Cancel</button>
+        <button
+          @click="closeModal"
+          class="button"
+        >
+          Cancel
+        </button>
         <button class="button is-danger">Delete</button>
       </footer>
     </div>
   </div>
 </template>
+
+<script setup>
+/*
+  props
+*/
+
+  const props = defineProps({
+    modelValue: {
+      type: Boolean,
+      default: false
+    }
+  })
+
+/*
+  emits
+*/
+
+  const emit = defineEmits(['update:modelValue'])
+
+/*
+  close modal
+*/
+
+  const closeModal = () => {
+    emit('update:modelValue', false)
+  }
+
+</script>
--- vue-composition-api-noteballs/src/components/Notes/Note.vue
+++ vue-composition-api-noteballs/src/components/Notes/Note.vue
@@ -28,6 +28,7 @@
     </footer>
     <ModalDeleteNote
       v-if="modals.deleteNote"
+      v-model="modals.deleteNote"
     />
   </div>
 </template>
