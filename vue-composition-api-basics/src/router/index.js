fbbd88dd4880da912370b8f151056c0f18ad45b8
Danny Connell <sendstufftodanny@gmail.com>
2022-02-25 17:44:40 +0000

    Fixed incorrect PostDetailView import path

--- vue-composition-api-basics/src/router/index.js
+++ vue-composition-api-basics/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ModalsView from '../views/ModalsView.vue'
 import PostsView from '../views/PostsView.vue'
-import PostDetailView from '../views/HomeView.vue'
+import PostDetailView from '../views/PostDetailView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
