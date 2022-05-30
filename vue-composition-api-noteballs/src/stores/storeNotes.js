292c044083dfd50931c5310a9b7eb067720fed61
Danny Connell <sendstufftodanny@gmail.com>
2022-05-30 10:15:51 +0100

    20.5

--- vue-composition-api-noteballs/src/stores/storeNotes.js
+++ vue-composition-api-noteballs/src/stores/storeNotes.js
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import { collection, onSnapshot, doc, setDoc, deleteDoc } from 'firebase/firestore'
+import { collection, onSnapshot, doc, setDoc, deleteDoc, updateDoc } from 'firebase/firestore'
 import { db } from '@/js/firebase'
 
 const notesCollectionRef = collection(db, 'notes')
@@ -44,9 +44,10 @@ export const useStoreNotes = defineStore('storeNotes', {
     async deleteNote(idToDelete) {
       await deleteDoc(doc(notesCollectionRef, idToDelete))
     },
-    updateNote(id, content) {
-      let index = this.notes.findIndex(note => note.id === id )
-      this.notes[index].content = content
+    async updateNote(id, content) {
+      await updateDoc(doc(notesCollectionRef, id), {
+        content
+      })
     }
   },
   getters: {
