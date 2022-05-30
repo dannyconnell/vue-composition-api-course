3db94abaa872e0638d05d09302b1371de6c992f9
Danny Connell <sendstufftodanny@gmail.com>
2022-05-30 08:34:40 +0100

    19.3

---
+++ vue-composition-api-noteballs/src/js/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDzOhY4FBznWEM8tC4zm47cHkjaW6Nf4J4',
+  authDomain: 'noteballs-22869.firebaseapp.com',
+  projectId: 'noteballs-22869',
+  storageBucket: 'noteballs-22869.appspot.com',
+  messagingSenderId: '452955415779',
+  appId: '1:452955415779:web:9882a55e2a853f5ff051d6'
+}
+
+const app = initializeApp(firebaseConfig)
