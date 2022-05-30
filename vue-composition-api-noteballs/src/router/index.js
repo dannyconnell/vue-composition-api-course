fce1464dcf5455710707252f40f079d703b74188
Danny Connell <sendstufftodanny@gmail.com>
2022-05-30 13:15:59 +0100

    21.1

--- vue-composition-api-noteballs/src/router/index.js
+++ vue-composition-api-noteballs/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import ViewNotes from '@/views/ViewNotes.vue'
 import ViewEditNote from '@/views/ViewEditNote.vue'
 import ViewStats from '@/views/ViewStats.vue'
+import ViewAuth from '@/views/ViewAuth.vue'
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path: '/stats',
     name: 'stats',
     component: ViewStats
+  },
+  {
+    path: '/auth',
+    name: 'auth',
+    component: ViewAuth
   }
 ]
 
---
+++ vue-composition-api-noteballs/src/views/ViewAuth.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="auth">
+    <div class="tabs is-centered">
+      <ul>
+        <li
+          :class="{ 'is-active' : !register }"
+        >
+          <a @click.prevent="register = false">Login</a>
+        </li>
+        <li
+          :class="{ 'is-active' : register }"
+        >
+          <a @click.prevent="register = true">Register</a>
+        </li>
+      </ul>
+    </div>
+
+    <div class="card auth-form">
+      <div class="card-content">
+        <div class="title has-text-centered">
+          {{ formTitle }}
+        </div>
+        <div class="content">
+          Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+/*
+  imports
+*/
+
+  import { ref, computed } from 'vue'
+
+/*
+  register / login
+*/
+
+  const register = ref(false)
+
+/*
+  form title
+*/
+
+  const formTitle = computed(() => {
+    return register.value ? 'Register' : 'Login'
+  })
+</script>
+
+<style>
+.auth-form {
+  max-width: 400px;
+  margin: 0 auto;
+}
+</style>
