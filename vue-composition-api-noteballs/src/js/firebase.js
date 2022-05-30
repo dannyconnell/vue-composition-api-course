69b78e361baa7bc92520696620e78ed2fc82163c
Danny Connell <sendstufftodanny@gmail.com>
2022-05-30 13:43:44 +0100

    21.3

--- vue-composition-api-noteballs/src/js/firebase.js
+++ vue-composition-api-noteballs/src/js/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDzOhY4FBznWEM8tC4zm47cHkjaW6Nf4J4',
@@ -12,7 +13,9 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
+const auth = getAuth(app)
 
 export {
-  db
+  db,
+  auth
 }
---
+++ vue-composition-api-noteballs/src/stores/storeAuth.js
@@ -0,0 +1,13 @@
+import { defineStore } from 'pinia'
+import { auth } from '@/js/firebase'
+
+export const useStoreAuth = defineStore('storeAuth', {
+  state: () => {
+    return { 
+
+    }
+  },
+  actions: {
+
+  }
+})
