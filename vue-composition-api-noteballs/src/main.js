a363205a21be6a10022c2870f395911cd480a780
Danny Connell <sendstufftodanny@gmail.com>
2022-05-31 13:15:49 +0100

    21.9

--- vue-composition-api-noteballs/src/main.js
+++ vue-composition-api-noteballs/src/main.js
@@ -1,9 +1,15 @@
-import { createApp } from 'vue'
+import { createApp, markRaw } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from '@/router'
 
+const pinia = createPinia()
+
+pinia.use(({ store }) => {
+  store.router = markRaw(router)
+})
+
 createApp(App)
-  .use(createPinia())
+  .use(pinia)
   .use(router)
   .mount('#app')
--- vue-composition-api-noteballs/src/stores/storeAuth.js
+++ vue-composition-api-noteballs/src/stores/storeAuth.js
@@ -14,8 +14,10 @@ export const useStoreAuth = defineStore('storeAuth', {
         if (user) {
           this.user.id = user.uid
           this.user.email = user.email
+          this.router.push('/')
         } else {
           this.user = {}
+          this.router.replace('/auth')
         }
       })
     },
